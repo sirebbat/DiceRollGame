80abac7be1c25847208bff755835363892e411f7
Rusbert Liriano <rubenliriano1988@gmail.com>
2015-09-20 15:21:16 -0400

    Initial
    Dice Roll Game loops through 100 times to and gets random number.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DiceRollGame
+{
+    partial class dice_roll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_roll = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.lv_dice_roll = new System.Windows.Forms.ListView();
+            this.lw_dice_one = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_dice_two = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lv_sequence = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btn_roll
+            // 
+            this.btn_roll.AutoSize = true;
+            this.btn_roll.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_roll.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+            this.btn_roll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_roll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_roll.Location = new System.Drawing.Point(234, 12);
+            this.btn_roll.Name = "btn_roll";
+            this.btn_roll.Size = new System.Drawing.Size(75, 65);
+            this.btn_roll.TabIndex = 1;
+            this.btn_roll.Text = "Roll";
+            this.btn_roll.UseVisualStyleBackColor = false;
+            this.btn_roll.Click += new System.EventHandler(this.btn_roll_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_cancel.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+            this.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.Location = new System.Drawing.Point(234, 322);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 62);
+            this.btn_cancel.TabIndex = 2;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // lv_dice_roll
+            // 
+            this.lv_dice_roll.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lv_dice_roll.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lw_dice_one,
+            this.lv_dice_two,
+            this.lv_sequence});
+            this.lv_dice_roll.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lv_dice_roll.FullRowSelect = true;
+            this.lv_dice_roll.GridLines = true;
+            this.lv_dice_roll.Location = new System.Drawing.Point(0, 0);
+            this.lv_dice_roll.Name = "lv_dice_roll";
+            this.lv_dice_roll.Size = new System.Drawing.Size(208, 396);
+            this.lv_dice_roll.TabIndex = 2;
+            this.lv_dice_roll.UseCompatibleStateImageBehavior = false;
+            this.lv_dice_roll.View = System.Windows.Forms.View.Details;
+            // 
+            // lw_dice_one
+            // 
+            this.lw_dice_one.Text = "Dice One";
+            // 
+            // lv_dice_two
+            // 
+            this.lv_dice_two.Text = "Dice Two";
+            // 
+            // lv_sequence
+            // 
+            this.lv_sequence.Text = "Sequence";
+            this.lv_sequence.Width = 70;
+            // 
+            // dice_roll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(357, 396);
+            this.Controls.Add(this.lv_dice_roll);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_roll);
+            this.Name = "dice_roll";
+            this.ShowIcon = false;
+            this.Text = "Dice Roll Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_roll;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.ListView lv_dice_roll;
+        private System.Windows.Forms.ColumnHeader lw_dice_one;
+        private System.Windows.Forms.ColumnHeader lv_dice_two;
+        private System.Windows.Forms.ColumnHeader lv_sequence;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceRollGame
+{
+    public partial class dice_roll : Form
+    {
+        public dice_roll()
+        {
+            InitializeComponent();
+        }
+
+        //Start of - Roll button - Clicked Event
+        private void btn_roll_Click(object sender, EventArgs e)
+
+        {
+            #region declaration 
+            //Declaration of variables
+            int dice_one = 0;   //Dice One
+            int dice_two = 0;   //Dice Two
+            int sequence = 0;   //Roll Sequence Number
+            Random rand = new Random();  //Random number
+            #endregion
+
+          
+            System.Media.SystemSounds.Hand.Play();
+            //Clears list view 
+
+         
+            lv_dice_roll.Items.Clear();
+
+            //Roll Dice
+            //Loops through one hundreed times
+            for (int counter_two = 1; counter_two <= 100; counter_two++)
+                {
+                    //Creates new list view items
+                    ListViewItem dice_item_one = new ListViewItem();
+                    
+                    // gets random number from 1-100
+                    dice_one = rand.Next(1, 101);
+                    dice_two = rand.Next(1, 101);
+
+                    //adds random numbers to list view items
+                    dice_item_one.Text = Convert.ToString(dice_one);
+                    dice_item_one.SubItems.Add(Convert.ToString(dice_two));
+
+                #region rollsequence
+
+                //Checks wether the dices match
+                if (dice_one == dice_two) {
+
+                        sequence++;
+
+                        //if matches match then adds the sequence number
+                        if (sequence == 1 || sequence == 21 || sequence == 31 || sequence == 41 || sequence == 51 || sequence == 61 || sequence == 71 || sequence == 81 || sequence == 91)
+                        {
+                            dice_item_one.SubItems.Add(sequence + "st"); //adds sequence number to list view items
+
+                        }
+
+                        else if (sequence == 2 || sequence == 22 || sequence == 32 || sequence == 42 || sequence == 52 || sequence == 62 || sequence == 72 || sequence == 82 || sequence == 92)
+                        {
+
+                            dice_item_one.SubItems.Add(sequence + "nd"); //adds sequence number to list view items
+
+
+                    }
+                        else if (sequence == 3 || sequence == 23 || sequence == 33 || sequence == 43 || sequence == 53 || sequence == 63 || sequence == 73 || sequence == 83 || sequence == 93)
+                        {
+
+                            dice_item_one.SubItems.Add(sequence + "rd"); //adds sequence number to list view items
+
+
+                    }
+
+                        else {
+
+                            dice_item_one.SubItems.Add(sequence + "th");
+
+
+                        }
+
+                     
+                    }
+# endregion
+                lv_dice_roll.Items.Add(dice_item_one); //adds list view items to the dice_roll list view
+
+
+
+
+
+            }
+               
+
+
+   
+
+
+
+
+        }
+        //-------END OF ROLL BUTTON-----\\
+
+        //------BEGINING OF CANCEL BUTTON\\
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //------END OF CANCEL BUTTON\\
+    }
+}
